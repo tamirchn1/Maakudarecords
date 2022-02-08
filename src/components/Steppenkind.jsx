e6819879257447bbef07396b2cdfa21c1f2ec0e6
Tamirchn1@gmail.com <T2151991t>
2022-02-08 08:48:09 +0100

    steppenkind iframe

--- src/components/Steppenkind.jsx
+++ src/components/Steppenkind.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import Iframe from "react-iframe";
 
 function Steppenkind() {
   return (
@@ -11,6 +12,15 @@ function Steppenkind() {
         <p>Steppenkind are our friends, we met them in a party</p>
       </div>
 
+      <Iframe
+        style="border: 0; width: 365px; height: 700px;"
+        src="https://bandcamp.com/EmbeddedPlayer/album=636602623/size=large/bgcol=333333/linkcol=e99708/transparent=true/"
+        seamless
+      >
+        <a href="https://steppenkind.bandcamp.com/album/walk-along-your-river">
+          Walk Along Your River by Steppenkind
+        </a>
+      </Iframe>
       <Footer />
     </div>
   );
